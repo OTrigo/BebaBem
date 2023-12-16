838841ec94d2fc0c5466b95c8880e149ef3a5c14
Jorge <jorge.coronado@acct.global>
2023-12-16 15:22:02 -0300

    refactor: refactoring code

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import Home from "./src/Pages/Home";
 import Notfication from "./src/services/Notification";
 import * as Notifications from "expo-notifications";
 import { useEffect } from "react";
-import Animation from "./src/services/Animation";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
